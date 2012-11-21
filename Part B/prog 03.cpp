597067fd638b7a4ba92685c01f7633139e4cb238
Akash Devaraju <akashdevaraju@gmail.com>
2012-11-21 13:49:59 +0530

    Added Prog 3 (Work in progress)

---
+++ Part B/prog 03.cpp
@@ -0,0 +1,61 @@
+// Author : SkyKOG
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+struct node 
+{
+	int dist[20];
+	int from[20];
+}rt[20];
+
+int main()
+{
+	system("clear");
+
+	int dmat[20][20];
+	int n=0,i,j,k,count;
+
+	cout<<"\nEnter The Number of Nodes : ";
+	cin>>n;
+
+	cout <<"\nEnter The Cost Matrix : ";
+	for (i = 1; i <= n ; i++ )
+ 	{
+ 		for (j = 1; j <= n ; j++ )
+ 		{
+ 			cin>>dmat[i][j];
+ 			dmat[i][i]=0;
+ 			rt[i].dist[j]=dmat[i][j];
+ 			rt[i].from[j]=j;
+ 		}	
+	}
+
+	for ( i = 1; i <= n ; i++ )
+	{
+		for ( j = 1; j <= n ; j++ )
+		{
+			for ( k = 1; k <= n ; k++ )
+			{
+				if (rt[i].dist[j] > rt[i].dist[k] + rt[k].dist[j])
+				{
+					rt[i].dist[j] = rt[i].dist[k] + rt[k].dist[j];
+					rt[i].from[j] = k;
+				}
+			}
+		}
+	}	
+
+	for ( i = 1 ; i <= n; i++)
+	{
+		cout<<"\nState value for router : "<<i<<"\n";
+		for ( i = 1; i <= n; i++)
+		{
+			cout<<"\nDestination Node : "<<j<<" Next Node : "<<rt[i].from[j]<<" Distance : "<<rt[i].dist[j]<<"\n";
+		}
+	}
+	return 0;
+}
